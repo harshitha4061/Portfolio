b6e42361d21bece22c16f35994a06161c0f4c145
harshitha4061 <harshu.kolli05@gmail.com>
2025-03-14 22:39:33 +0530

    responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div>
             <div className="w-full flex flex-row justify-between items-center sm:px-16 px-5 bg-[#121110]">
-                <p className="p-5 text-xl text-white font-bold hover:scale-105 transition duration-200">Kolli Harshitha</p>
+                <p className="py-5 text-xl text-white font-bold hover:scale-105 transition duration-200">Kolli Harshitha</p>
 
                 <div onClick={() => setIsOpen(!isOpen)} className="text-white text-2xl sm:hidden">
                     {isOpen ? <FiX /> : <FiMenu />}
@@ -19,7 +19,7 @@ const Navbar = () => {
                     <li className='p-5 text-sm text-white font-extralight cursor-pointer hover:text-gray-500 hover:scale-110 hover:underline underline-offset-6 transition duration-200'>Education</li>
                     <li className='p-5 text-sm text-white font-extralight cursor-pointer hover:text-gray-500 hover:scale-110 hover:underline underline-offset-6 transition duration-200'>Projects</li>
                     <li className='p-5 text-sm text-white font-extralight cursor-pointer hover:text-gray-500 hover:scale-110 hover:underline underline-offset-6 transition duration-200'>Experience</li>
-                    <li className='p-5 text-sm text-white font-extralight cursor-pointer hover:text-gray-500 hover:scale-110 hover:underline underline-offset-6 transition duration-200'>Contact</li>
+                    <li className='pr-0 p-5  text-sm text-white font-extralight cursor-pointer hover:text-gray-500 hover:scale-110 hover:underline underline-offset-6 transition duration-200'>Contact</li>
                 </ul>
             </div>
             <div>
--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -4,12 +4,12 @@ const Side = () => {
     return (
         <div>
             <ul className="flex flex-col bg-black w-full">
-                <li className='p-5  px-10  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>About Me</li>
-                <li className='p-5 px-10  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Skills</li>
-                <li className='p-5  px-10 text-sm text-white font-extralight cursor-pointer border-y border-gray-300  hover:text-gray-500 transition duration-200'  >Education</li>
-                <li className='p-5 px-10  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Projects</li>
-                <li className='p-5 px-10  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Experience</li>
-                <li className='p-5  px-10 text-sm text-white font-extralight cursor-pointer border-y border-gray-300  hover:text-gray-500 transition duration-200'  >Contact</li>
+                <li className='p-5  px-5  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>About Me</li>
+                <li className='p-5 px-5  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Skills</li>
+                <li className='p-5  px-5 text-sm text-white font-extralight cursor-pointer border-y border-gray-300  hover:text-gray-500 transition duration-200'  >Education</li>
+                <li className='p-5 px-5  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Projects</li>
+                <li className='p-5 px-5  text-sm text-white font-extralight cursor-pointer   border-y border-gray-300  hover:text-gray-500 transition duration-200'>Experience</li>
+                <li className='p-5  px-5 text-sm text-white font-extralight cursor-pointer border-y border-gray-300  hover:text-gray-500 transition duration-200'  >Contact</li>
             </ul>
         </div>
     )
