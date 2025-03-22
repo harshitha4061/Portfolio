879cb6965a335cf744e5103a609e70d061e91d62
harshitha4061 <harshu.kolli05@gmail.com>
2025-03-23 01:49:20 +0530

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 import { motion } from 'motion/react'
 import { easeInOut } from "motion"
 import { FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 import { FaLinkedin } from "react-icons/fa";
+import { SiHackerrank } from "react-icons/si";
+import { SiGeeksforgeeks } from "react-icons/si";
+
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = { name, email, message };
+
+    try {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        alert("Message sent successfully!");
+        setName("");
+        setEmail("");
+        setMessage("");
+      } else {
+        alert("Failed to send message.");
+      }
+    } catch (error) {
+      alert("Error sending message.");
+    }
+  };
+
   return (
     <div id="contact">
       <div className='w-full h-auto bg-[#1B1A19] p-8 sm:p-16 '>
         <div className='w-full h-full flex flex-row  justify-between flex-wrap'>
-          <div className='w-full sm:w-[40%] h-full flex flex-col flex-wrap p-4 sm:p-0'>
+          <div className='w-full sm:w-[45%] h-full flex flex-col flex-wrap p-4 sm:p-0'>
             <motion.h1 className="text-2xl sm:text-4xl text-left  text-orange-500"
               initial={{ opacity: 0, x: -20 }}
               whileInView={{ opacity: 1, x: 0 }}
@@ -23,69 +56,84 @@ const Contact = () => {
               transition={{ duration: 1, delay: 1, ease: easeInOut }}>GET IN TOUCH</motion.h1>
             <div className='w-full h-auto flex flex-row justify-between flex-wrap'>
               <div>
-                <h1 className="text-2xl  text-left text-white">CONTACT</h1>
+                <h1 className="text-xl sm:text-2xl  text-left text-white">CONTACT</h1>
                 <h1 className="text-lg  text-left text-blue-400 py-2">harshu.kolli05@gmail.com</h1>
               </div>
-              <div>
-                <h1 className="text-2xl  text-left text-white ">SOCIAL MEDIA</h1>
-                <div className='flex flex-row text-4xl text-white justify-between p-2'>
+              <div className='flex flex-col items-start sm:items-center'>
+                <h1 className="text-xl sm:text-2xl  text-left text-white ">SOCIAL MEDIA</h1>
+                <div className='flex flex-row text-4xl text-white justify-between py-2 gap-4'>
                   <a href="https://github.com/harshitha4061" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                   </a>
                   <a href="https://leetcode.com/u/harshu_kolli05/" target="_blank" rel="noopener noreferrer">
                     <SiLeetcode />
                   </a>
-                  <a href="www.linkedin.com/in/kolli-harshitha-b95006293" target="_blank" rel="noopener noreferrer">
+                  <a href="https://www.linkedin.com/in/kolli-harshitha-b95006293/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedin />
+                  </a>
+                  <a href="https://www.hackerrank.com/profile/harshu_kolli05" target="_blank" rel="noopener noreferrer">
+                    <SiHackerrank />
+                  </a>
+                  <a href="https://www.geeksforgeeks.org/user/harshuk2knb/?_gl=1*3yswcp*_up*MQ..*_gs*MQ..&gclid=Cj0KCQjwv_m-BhC4ARIsAIqNeBte0ftDrXv0sNOU5foFPV6mNYonEyxbWL7q3Y-F9gNr14LFbL9wnvsaAhsrEALw_wcB" target="_blank" rel="noopener noreferrer">
+                    <SiGeeksforgeeks />
                   </a>
                 </div>
               </div>
             </div>
           </div>
-          <div className='flex flex-col items-center h-full w-full sm:w-[50%]'>
-            <motion.h1 className=' text-3xl self-start p-4 text-orange-500'
+          <div className='flex flex-col items-center h-full w-full sm:w-[45%] mx-5'>
+            <motion.h1 className='text-2xl sm:text-3xl self-start py-4 sm:p-4 text-orange-500'
               initial={{ opacity: 0, x: -20 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
               transition={{ duration: 1, delay: 0.5, ease: easeInOut }}>CONTACT FORM</motion.h1>
 
-            <div className='flex flex-col w-full p-4'>
-              <form action="/contactform" method="POST" className="space-y-4">
+            <div className='flex flex-col w-full p-0 sm:p-4 '>
+              <form onSubmit={handleSubmit} className="space-y-4">
                 <div className="flex flex-col">
-                  <label htmlFor="name" className='text-white text-xl mb-3'>Name</label>
+                  <label htmlFor="name" className='text-white text-lg sm:text-xl mb-3'>Name</label>
                   <input
                     type="text"
                     id="name"
                     name="name"
                     className='p-2 rounded-2xl border-[0.5px] border-white text-white bg-[#1B1A19] '
                     placeholder="Your Name"
+                    autoComplete="name"
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}
                   />
                 </div>
 
                 <div className="flex flex-col">
-                  <label htmlFor="email" className='text-white text-xl mb-3'>Email</label>
+                  <label htmlFor="email" className='text-white text-lg sm:text-xl mb-3'>Email</label>
                   <input
                     type="email"
                     id="email"
                     name="email"
                     className='p-2 rounded-2xl border-[0.5px] border-white text-white bg-[#1B1A19] '
                     placeholder="Your Email"
+                    autoComplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
                 </div>
 
                 <div className="flex flex-col">
-                  <label htmlFor="message" className='text-white text-xl mb-3'>Message</label>
+                  <label htmlFor="message" className='text-white text-lg sm:text-xl mb-3'>Message</label>
                   <textarea
                     id="message"
                     name="message"
                     className='p-2 rounded-2xl border-[0.5px] border-white text-white bg-[#1B1A19] '
                     placeholder="Your Message"
                     rows="4"
+                    autoComplete="off"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                   />
                 </div>
 
                 <div className="flex self-start">
-                  <button type="submit" className="bg-blue-600 text-white text-lg px-6 py-3 rounded-2xl hover:bg-blue-800 transition-colors">
+                  <button type="submit" className="bg-blue-600 text-white text-md sm:text-lg px-6 py-3 rounded-2xl hover:bg-blue-800 transition-colors">
                     Send
                   </button>
                 </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -43,7 +43,7 @@ const Project = () => {
             </div>
             <div className='flex flex-col p-3'>
               <div className="flex justify-end">
-                <a href="https://github.com/harshitha4061/Fitwave_Project" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/harshitha4061/Lost_and_Found" target="_blank" rel="noopener noreferrer">
                   <FaExternalLinkAlt className="text-white text-xl hover:text-gray-600 transition duration-200" />
                 </a>
               </div>
@@ -59,77 +59,17 @@ const Project = () => {
           viewport={{ once: true }}
           transition={{duration:2,ease:easeInOut}}>
             <div className="w-full aspect-[16/9]">
-              <img src="lostandfound.png" alt="Lost and Found" className="w-full h-full object-cover" />
+              <img src="DOT.jpeg" alt="Lost and Found" className="w-full h-full object-cover" />
             </div>
             <div className='flex flex-col p-3'>
               <div className="flex justify-end">
-                <a href="https://github.com/harshitha4061/Fitwave_Project" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/harshitha4061/DELIVER-ON-TIME/tree/main" target="_blank" rel="noopener noreferrer">
                   <FaExternalLinkAlt className="text-white text-xl hover:text-gray-600 transition duration-200" />
                 </a>
               </div>
               <div className='p-2'>
-                <p className='text-xl text-center text-white'>Lost and Found</p>
-                <p className='text-center text-white text-xs'>(HTML, CSS, JavaScript, Express.js, Node.js, MongoDB)</p>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div className="w-83 h-75 border-[0.5px] border-white rounded-xl bg-[#1B1A19]  overflow-hidden m-5 hover:scale-105"
-          initial={{opacity:0,y:10}}
-          whileInView={{opacity:1,y:0}}
-          viewport={{ once: true }}
-          transition={{duration:2,ease:easeInOut}}>
-            <div className="w-full aspect-[16/9]">
-              <img src="lostandfound.png" alt="Lost and Found" className="w-full h-full object-cover" />
-            </div>
-            <div className='flex flex-col p-3'>
-              <div className="flex justify-end">
-                <a href="https://github.com/harshitha4061/Fitwave_Project" target="_blank" rel="noopener noreferrer">
-                  <FaExternalLinkAlt className="text-white text-xl hover:text-gray-600 transition duration-200" />
-                </a>
-              </div>
-              <div className='p-2'>
-                <p className='text-xl text-center text-white'>Lost and Found</p>
-                <p className='text-center text-white text-xs'>(HTML, CSS, JavaScript, Express.js, Node.js, MongoDB)</p>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div className="w-83 h-75 border-[0.5px] border-white rounded-xl bg-[#1B1A19]  overflow-hidden m-5 hover:scale-105"
-          initial={{opacity:0,y:10}}
-          whileInView={{opacity:1,y:0}}
-          viewport={{ once: true }}
-          transition={{duration:2,ease:easeInOut}}>
-            <div className="w-full aspect-[16/9]">
-              <img src="fitwave.jpeg" alt="Fitwave" className="w-full h-full object-cover" />
-            </div>
-            <div className='flex flex-col p-3'>
-              <div className="flex justify-end">
-                <a href="https://github.com/harshitha4061/Fitwave_Project" target="_blank" rel="noopener noreferrer">
-                  <FaExternalLinkAlt className="text-white text-xl hover:text-gray-600 transition duration-200" />
-                </a>
-              </div>
-              <div className='p-2'>
-                <p className='text-xl text-center text-white'>Fitwave</p>
-                <p className='text-center text-white text-sm'>(HTML, CSS, JavaScript, Flask ,MongoDB)</p>
-              </div>
-            </div>
-          </motion.div>
-          <motion.div className="w-83 h-75 border-[0.5px] border-white rounded-xl bg-[#1B1A19]  overflow-hidden m-5 hover:scale-105"
-          initial={{opacity:0,y:10}}
-          whileInView={{opacity:1,y:0}}
-          viewport={{ once: true }}
-          transition={{duration:2,ease:easeInOut}}>
-            <div className="w-full aspect-[16/9]">
-              <img src="fitwave.jpeg" alt="Fitwave" className="w-full h-full object-cover" />
-            </div>
-            <div className='flex flex-col p-3'>
-              <div className="flex justify-end">
-                <a href="https://github.com/harshitha4061/Fitwave_Project" target="_blank" rel="noopener noreferrer">
-                  <FaExternalLinkAlt className="text-white text-xl hover:text-gray-600 transition duration-200" />
-                </a>
-              </div>
-              <div className='p-2'>
-                <p className='text-xl text-center text-white'>Fitwave</p>
-                <p className='text-center text-white text-sm'>(HTML, CSS, JavaScript, Flask ,MongoDB)</p>
+                <p className='text-xl text-center text-white'>Deliver on Time</p>
+                <p className='text-center text-white text-xs'>(Python, Pandas, NumPy, Seaborn, and Matplotlib)</p>
               </div>
             </div>
           </motion.div>
